f6b9ff4079d5b7ee094d302c1c09c546155e1e67
takoyummy <takoyummy@naver.com>
2024-09-25 23:01:21 +0900

    feat: 사용자 포인트 내역 조회 서비스 로직 추가

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -1,13 +1,17 @@
 package io.hhplus.tdd.point;
 
 import io.hhplus.tdd.database.UserPointTable;
+import io.hhplus.tdd.database.PointHistoryTable;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class PointService {
     private final UserPointTable userPointTable;
+    private final PointHistoryTable pointHistoryTable;
 
     /**
      * 사용자의 포인트 정보를 조회합니다.
@@ -16,6 +20,15 @@ public class PointService {
      */
     public UserPoint getUserPoint(final long userId) {
         return userPointTable.selectById(userId);
+    }
+
+    /**
+     * 사용자의 포인트 내역을 조회합니다.
+     * @param userId
+     * @return List<PointHistory>
+     */
+    public List<PointHistory> getUserPointHistories(final long userId) {
+        return pointHistoryTable.selectAllByUserId(userId);
     }
 
 }
